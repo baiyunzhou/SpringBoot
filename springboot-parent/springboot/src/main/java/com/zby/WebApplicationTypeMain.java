dc14340816ac42b1c50185fb1efdc47b37a18ce5
zhoubaiyun <zhoubaiyun@lixin360.com>
2018-11-19 18:12:13 +0800

    init

--- springboot-parent/springboot/src/main/java/com/zby/WebApplicationTypeMain.java
+++ springboot-parent/springboot/src/main/java/com/zby/WebApplicationTypeMain.java
@@ -1,13 +1,21 @@
 package com.zby;
 
+import java.lang.reflect.Method;
+
+import org.springframework.boot.WebApplicationType;
 import org.springframework.context.ApplicationContext;
 
 public class WebApplicationTypeMain {
 
-	public static void main(String[] args) {
-		WebApplicationType classpathWwebApplicationType = WebApplicationType.deduceFromClasspath();
+	public static void main(String[] args) throws Exception {
+		Method deduceFromClasspath = WebApplicationType.class.getDeclaredMethod("deduceFromClasspath");
+		deduceFromClasspath.setAccessible(true);
+		WebApplicationType classpathWwebApplicationType = (WebApplicationType) deduceFromClasspath.invoke(null);
 		System.out.println("classpathWwebApplicationType:" + classpathWwebApplicationType);
-		WebApplicationType applicationContexyWebApplicationType = WebApplicationType.deduceFromApplicationContext(ApplicationContext.class);
+		Method deduceFromApplicationContext = WebApplicationType.class.getDeclaredMethod("deduceFromApplicationContext", Class.class);
+		deduceFromApplicationContext.setAccessible(true);
+		WebApplicationType applicationContexyWebApplicationType = (WebApplicationType) deduceFromApplicationContext.invoke(null,
+				ApplicationContext.class);
 		System.out.println("applicationContextWebApplicationType:" + applicationContexyWebApplicationType);
 	}
 
